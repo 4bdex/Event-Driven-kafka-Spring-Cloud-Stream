298a7b4b26952ee6b31cf3030eea5f2318644765
Abdex <abdex4social@gmail.com>
2025-10-18 16:15:57 +0100

    init

---
+++ src/main/java/ma/enset/event_driven_kafka_spring_cloud_stream/EventDrivenKafkaSpringCloudStreamApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.event_driven_kafka_spring_cloud_stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenKafkaSpringCloudStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDrivenKafkaSpringCloudStreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/enset/event_driven_kafka_spring_cloud_stream/EventDrivenKafkaSpringCloudStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.event_driven_kafka_spring_cloud_stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDrivenKafkaSpringCloudStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
